ca5f1308c35d2840fb10fd43b3dc1d75683734d1
unknown <johannes.guetling@outlook.com>
2020-02-27 13:45:22 +0100

    test header

--- js/main.js
+++ js/main.js
@@ -64,6 +64,12 @@ fetch('https://gtlng.github.io/mvheustreu/js/htmlcomponents/header.html').then(f
     // console.log(html);
     document.getElementById("header").innerHTML = html;
     //newHTML = html;
+    (function (window, document) {
+        document.getElementById('toggle').addEventListener('click', function (e) {
+            document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+            document.getElementById('toggle').classList.toggle('x');
+        });
+    })(this, this.document);
 }).catch(function (err) {
     // There was an error
     console.warn('Something went wrong.', err);
